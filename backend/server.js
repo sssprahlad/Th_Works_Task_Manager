5a7ebb9f18ba2d0342cfa5b6d3a8a4b43601644d
sssprahlad <k.sssprahlad@gmail.com>
2025-11-01 08:21:29 +0530

    be changes

--- backend/server.js
+++ backend/server.js
@@ -9,16 +9,40 @@ require("dotenv").config();
 
 
 const app = express();
-app.use(express.json());
+// app.use(express.json());
+
+const allowedOrigins = [
+     'https://th-works-task-manager.vercel.app/',
+  'http://localhost:3000'
+ 
+];
 
 
-app.use(cors({
-    origin: "https://th-works-task-manager.vercel.app/" || "http://localhost:3000",
-    credentials:true,
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    method:["GET","POST","PATCH","DELETE"]
+ const corsOptions = {
+    origin: function(origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+    methods: ['GET','POST','PUT','DELETE','PATCH','OPTIONS'],
+    allowedHeaders: ['Content-Type','Authorization'],
+    optionsSuccessStatus: 204
+  };
 
-}));
+  app.use(cors(corsOptions));
+// app.options('/*', cors(corsOptions));
+app.use(express.json()); 
+
+// app.use(cors({
+//     origin: || "http://localhost:3000",
+//     credentials:true,
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+//     method:["GET","POST","PATCH","DELETE"]
+
+// }));
 
 
 app.use("/api", authenticationRouter);
